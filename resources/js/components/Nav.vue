a17805db56aaef329b4acd82107888b0ac1dde1c
sg208 <50724006+sg208@users.noreply.github.com>
2022-10-18 14:31:48 +0200

    Mobile first nav with icons

--- resources/js/components/Nav.vue
+++ resources/js/components/Nav.vue
@@ -1,16 +1,29 @@
 <template>
     <nav>
-        <ul class="flex flex-row flex-nowrap flex-grow space-x-5">
-            <li class="w-full font-bold text-base">
-                <router-link to="/">Home</router-link>
-            </li>
-            <li class="w-full font-bold text-base">
-                <router-link to="/login">Login</router-link>
+        <ul class="flex flex-col lg:flex-row gap-3 lg:gap-5">
+            <li
+                v-for="(route, index) in routes"
+                :key="index"
+                class="w-full font-bold text-base"
+            >
+                <ButtonAndLink
+                    :to="route.to"
+                    :icon="route.icon"
+                    :aria-label="route.ariaLabel"
+                    classes="!p-1 lg:!p-2"
+                />
             </li>
         </ul>
     </nav>
 </template>
 
 <script setup>
-// TODO: Need to loop the nav
+const routes = [
+    { to: "/", icon: "fa-solid fa-house", ariaLabel: "Back to homepage" },
+    {
+        to: "/login",
+        icon: "fa-solid fa-user",
+        ariaLabel: "User login to manage",
+    },
+];
 </script>
